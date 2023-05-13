2fdce75d3c2d146ee1a9f5dfa63847c3552bfd74
Gilass <gauthier.schmitt99@gmail.com>
2023-05-13 09:06:09 +0200

    index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.base')
+
+
+@section('content_top')
+    <h1 class="text-9xl font-extralight text-center my-10 text-rose-600">Welcome</h1>
+    <h1 class="text-7xl font-extralight text-center mb-10 text-rose-600"><i class="bi bi-chevron-double-down mr-5"></i>
+        Menu <i class="bi bi-chevron-double-down ml-5"></i></h1>
+@endsection
+
+@section('menu')
+@endsection
