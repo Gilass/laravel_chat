2c0297b34945f1ff2f613223c0a32096cfd3e910
Gilass <gauthier.schmitt99@gmail.com>
2023-05-13 14:19:06 +0200

    user_typing rm , because spend too much request in pusher 😢😢

--- resources/js/components/ChatForm.vue
+++ resources/js/components/ChatForm.vue
@@ -1,4 +1,7 @@
 <template>
+    <div>
+        <span v-for="user_typing in users_typing">user_typing</span>
+    </div>
     <div class="flex">
         <input
             id="btn-input"
@@ -7,10 +10,11 @@
             class="p-2 px-4 w-full bg-slate-800 rounded-l-xl transition focus:outline-none border-2 border-transparent focus:border-sky-500"
             placeholder="Type your message here..."
             v-model="newMessage"
-        @keyup.enter="sendMessage"
+            @keyup.enter="sendMessage"
         />
         <span>
-      <button class="h-full px-5 bg-sky-500 rounded-r-xl hover:bg-sky-400 focus:outline-none" id="btn-chat" @click="sendMessage">
+      <button class="h-full px-5 bg-sky-500 rounded-r-xl hover:bg-sky-400 focus:outline-none" id="btn-chat"
+              @click="sendMessage">
         Send
       </button>
     </span>
@@ -19,12 +23,6 @@
 </template>
 <script>
 export default {
-    props: ["user"],
-    data() {
-        return {
-            newMessage: "",
-        };
-    },
     methods: {
         sendMessage() {
             this.$emit("messagesent", {
@@ -34,5 +32,13 @@ export default {
             this.newMessage = "";
         },
     },
+    data() {
+        return {
+            newMessage: "",
+            users_typing: [],
+            timeoutInput: null,
+        };
+    },
+    props: ["user"]
 };
 </script>
